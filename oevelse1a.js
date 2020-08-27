b54180ed9d72b5e5bc19aa2df8c44285aa006c3a
Felix Bryld <felixbryld@Felixs-MacBook-Pro.local>
2020-08-27 09:03:17 +0200

    oevelses filer upload test

---
+++ oevelse1a.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', myfunction() {
+  console.log('page is fully loaded');
+}
+function myfunction() {
+    console.log("hello world");
+}
+
+
+console.log("hello");
+    const overskrift = document.querySelector("h1");
+    console.log(overskrift.textContent;);
+    overskrift.textContent; = "billeder";
+    console.log(overskrift.textContent;);
